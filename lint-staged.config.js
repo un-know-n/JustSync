fce0d43fd45a1d2d9810cad5db340521a8f064f2
un-know-n <fa1thlezss@gmail.com>
2023-06-23 23:01:27 +0300

    feat(general): add husky, lint-staged and commitlint

---
+++ lint-staged.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  '{apps,libs,tools}/**/*.{ts,tsx}': (files) => {
+    return `nx affected --target=typecheck --files=${files.join(',')}`;
+  },
+  '{apps,libs,tools}/**/*.{js,jsx,ts,tsx}': [
+    (files) => `nx affected:lint --files=${files.join(',')}`,
+    (files) => `nx format:write --files=${files.join(',')}`,
+    (files) => 'prettier --write',
+  ],
+};
